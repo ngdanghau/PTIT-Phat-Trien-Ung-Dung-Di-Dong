93f7240ecc51806db2aa15ec42bbec97e6497852
ngdanghau <ngdghau201@gmail.com>
2022-05-12 11:15:11 +0700

    chỉnh lại icon màu trắng

--- app/src/main/java/com/example/prudentialfinance/Activities/General/GoalActivity.java
+++ app/src/main/java/com/example/prudentialfinance/Activities/General/GoalActivity.java
@@ -174,7 +174,7 @@ public class GoalActivity extends AppCompatActivity {
             public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
                 new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
                         .addBackgroundColor(ContextCompat.getColor(GoalActivity.this, R.color.colorRed))
-                        .addActionIcon(R.drawable.ic_baseline_close_24)
+                        .addActionIcon(R.drawable.ic_close)
                         .create()
                         .decorate();
 
