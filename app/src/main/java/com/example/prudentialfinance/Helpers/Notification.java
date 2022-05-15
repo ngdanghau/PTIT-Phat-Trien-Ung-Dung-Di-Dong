3ed7912386e0c82a1b68f47209c29b4b665c657c
ngdanghau <ngdghau201@gmail.com>
2022-05-15 19:54:30 +0700

    Fix lỗi notification trên api mới

--- app/src/main/java/com/example/prudentialfinance/Helpers/Notification.java
+++ app/src/main/java/com/example/prudentialfinance/Helpers/Notification.java
@@ -2,10 +2,12 @@ package com.example.prudentialfinance.Helpers;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -24,26 +26,45 @@ public class Notification {
      *
      */
     @SuppressLint("UnspecifiedImmutableFlag")
-    public void showNotification(Activity activityDepartment, String title, String content)
+    public void showNotification(Activity ctx, String title, String content)
     {
+        int NOTIFICATION_ID = 234;
+        NotificationManager notificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        String CHANNEL_ID = "";
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CHANNEL_ID = "com.example.prudentialfinance";
+            CharSequence name = "prudential_finance";
+            String Description = "Prudential Finance";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+
         /*Step 1*/
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(activityDepartment)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(ctx, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_baseline_message_24)
                 .setContentTitle(title)
                 .setContentText(content)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         /*Step 2*/
-        Intent intent = new Intent(activityDepartment, HomeActivity.class);
+        Intent intent = new Intent(ctx, HomeActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         /*Step 3*/
          PendingIntent pendingIntent =
-                PendingIntent.getActivity(activityDepartment, 0,intent,PendingIntent.FLAG_UPDATE_CURRENT );
+                PendingIntent.getActivity(ctx, 0,intent, PendingIntent.FLAG_UPDATE_CURRENT );
          builder.setContentIntent(pendingIntent);
 
          /*Step 4*/
-        NotificationManagerCompat manager = NotificationManagerCompat.from(activityDepartment);
-        manager.notify(0, builder.build());
+        NotificationManagerCompat manager = NotificationManagerCompat.from(ctx);
+        manager.notify(NOTIFICATION_ID, builder.build());
     }
 }
